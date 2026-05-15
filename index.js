ce41349bfa37b31bf720fd7cd008357c04c09c37
David Morera Mondejar <david.morera@students.salle.url.edu>
2026-05-15 10:27:24 +0200

    add: new forms and fix problem with sizing

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import { EtherDream } from "@laser-dac/ether-dream";
 import { Circle, Line, Scene, Svg, loadSvgFile } from "@laser-dac/draw";
 import * as path from "path";
 import { io } from "socket.io-client";
+import { Rect } from "@laser-dac/draw";
 
 const __dirname = import.meta.dirname;
 console.log(__dirname);
@@ -40,14 +41,59 @@ socket.on("userDisconnected", (data) => {
       const normalizedX = user.x / 802.95;
       const normalizedY = user.y / 802.95;
       const normalizedRadius = user.radius / 802.95;
+      const color = [1, 1, 1];
+
+      if (user.color === "red") {
+        color[0] = 1;
+        color[1] = 0;
+        color[2] = 0;
+      }
+      if (user.color === "green") {
+        color[0] = 0;
+        color[1] = 1;
+        color[2] = 0;
+      }
+      if (user.color === "blue") {
+        color[0] = 0;
+        color[1] = 0;
+        color[2] = 1;
+      }
+
       if (user.shape === "circle") {
         const circle = new Circle({
           x: normalizedX,
           y: normalizedY,
           radius: normalizedRadius,
-          color: user.color === "red" ? [1, 0, 0] : [1, 1, 1],
+          color: color,
         });
         scene.add(circle);
+      }
+      if (user.shape === "square") {
+        const size = normalizedRadius * 2;
+        const halfSize = size / 2;
+        const square = new Rect({
+          x: normalizedX - halfSize,
+          y: normalizedY - halfSize,
+          width: size,
+          height: size,
+          color: color,
+        });
+        scene.add(square);
+      }
+      if (user.shape === "triangle") {
+        const size = normalizedRadius * 2;
+        const halfSize = size / 2;
+        const p1 = { x: normalizedX, y: normalizedY - halfSize };
+        const p2 = { x: normalizedX - halfSize, y: normalizedY + halfSize };
+        const p3 = { x: normalizedX + halfSize, y: normalizedY + halfSize };
+
+        scene.add(
+          new Line({ from: p1, to: p2, color: color, blankBefore: true }),
+        );
+        scene.add(new Line({ from: p2, to: p3, color: color }));
+        scene.add(
+          new Line({ from: p3, to: p1, color: color, blankAfter: true }),
+        );
       }
     }
   }
